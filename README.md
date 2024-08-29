6c019846c71490744e4932b13f79736a8974316c
Ahmed Galadima <ahmedgaladima05@gmail.com>
2024-08-29 06:17:15 +0100

    DID: ADD CONVENTION FOR CONST AND HTML UI PLUS ADD README INFO

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RockPaperScissors
-The Odin Project R-P-S Game 
+
+## The Odin Project R-P-S Game
+
+### Here Is The Link To The Website [https://sardauna005.github.io/RockPaperScissors/]
--- rps.js
+++ rps.js
@@ -1,9 +1,9 @@
 // Global Variables
 // In Rock-Paper-Scissors there are 1/9 outcomes and 3 of these outcomes are draws so a 1/6 that the choices are different.
 // therefore at hardcoding we might be looking at +9 if else statements unless i can find another way lol.
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
 let round = 5;
 let humanScore = 0;
 let computerScore = 0;
@@ -15,9 +15,9 @@ function playRound(humanChoice, computerChoice) {
     console.log(`${humanChoice} and ${computerChoice}: Draw`);
   } else if (
     // here we account for all situations where the user wins
-    (humanChoice === rock && computerChoice === scissors) ||
-    (humanChoice === paper && computerChoice === rock) ||
-    (humanChoice === scissors && computerChoice === paper)
+    (humanChoice === ROCK && computerChoice === SCISSORS) ||
+    (humanChoice === PAPER && computerChoice === ROCK) ||
+    (humanChoice === SCISSORS && computerChoice === PAPER)
   ) {
     console.log(`${humanChoice} and ${computerChoice}: You Win`);
     humanScore++;
@@ -35,11 +35,11 @@ function getComputerChoice() {
   let rand = Math.random() * 4; // gen random number between 0 and 1(1 excluded)
   let word;
   if (rand >= 1 && rand < 2) {
-    word = "rock";
+    word = ROCK;
   } else if (rand >= 2 && rand < 3) {
-    word = "paper";
+    word = PAPER;
   } else {
-    word = "scissors";
+    word = SCISSORS;
   }
   return word;
 }
@@ -47,7 +47,7 @@ function getComputerChoice() {
 // this code collects the users input and returns it if amongst the r-p-s choices.
 function getHumanChoice() {
   let word = prompt("Rock-Paper-Scissors?: ").toLowerCase();
-  if (word === "rock" || word === "paper" || word === "scissors") {
+  if (word === ROCK || word === PAPER || word === SCISSORS) {
     return word;
   } else {
     return console.log("Invalid Answer!");
